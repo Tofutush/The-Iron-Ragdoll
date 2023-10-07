a65f94acfa5bdaa9d7286f486e6cee2d9bc8b2c6
Tofutush <siroccoxxq@gmail.com>
2023-10-07 18:23:24 +0800

    g.a.stuff

--- page.js
+++ page.js
@@ -15,10 +15,7 @@
 // authors notes
 const authorsNotes = {
 	1: `<p>This is the start of The Iron Ragdoll! Thanks a lot for your willingness to give this a shot!</p>
-		<p>The Iron Ragdoll is also available on MSPFA, Webtoons, Tapas, Globalcomix, and ComicFury (see dropdown menu when hovering "Other" above). It would be so kind of you subscribe and leave comments!</p>`,
-	3: `<p>Inconsistency Alert!!! Sparky keeps toggling between sitting in the left seat and sitting in the right. Sorry guys, but geez is this stuff hard. I do my best.</p>
-		<p>Also, Sparky isn't wearing a seat belt the entire time. Please do buckle up.</p>`,
-	4: `<p>They are not family. They look like family, but those two people are only like two years older than Sparky. You will meet them soon.</p>`,
+		<p>The Iron Ragdoll is also available on MSPFA, GlobalComix, ComicFury, NamiComi, Tapas, and Webtoons (see dropdown menu when hovering "Other" above). It would be so kind of you subscribe and leave comments!</p>`,
 	6: `<p>Okay, so that was like, a <i>really</i> quick representation of Hillslope. There's definitely going to be more stuff besides grass, buildings and a single road. But the important landmarks are there: two apartments, two small house-like things, and an abandoned, broken building.</p>`,
 	8: `<p>Don't bother looking; the anklet is covered up by Sparky's huge pant sleeves. This is not an excuse for poor drawing (although the drawing <i>is</i> poor). It's actually her pants.</p>`,
 	11: `<p>This page is drawn in Clip Studio Paint. Drawing comics in SAI was horrible. I will eventually redraw the previous ten pages with CSP too, but right now it's too early to be turning back.</p>`,
