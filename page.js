6c1a6eca9dd56ad84575237ee1a65339f0f575d1
Tofutush <siroccoxxq@gmail.com>
2024-02-23 09:59:04 +0800

    change font etc

--- page.js
+++ page.js
@@ -1,7 +1,8 @@
 // authors notes
 const authorsNotes = {
 	1: `<p>This is the start of The Iron Ragdoll! Thanks a lot for your willingness to give this a shot!</p>
-		<p>The Iron Ragdoll is also available on MSPFA, GlobalComix, ComicFury, NamiComi, Tapas, and Webtoons (see dropdown menu when hovering "Other" above). It would be so kind of you subscribe and leave comments!</p>`,
+		<p>The Iron Ragdoll is also available on other platforms (see dropdown menu when hovering "Other" above). It would be so kind of you subscribe and leave comments!</p>`,
+	4: `<p>Sparky settles in her new home. She knows it's supposed to be bad, but somehow it's worse.</p>`,
 	6: `<p>Okay, so that was like, a <i>really</i> quick representation of Hillslope. There's definitely going to be more stuff besides grass, buildings and a single road. But the important landmarks are there: two apartments, two small house-like things, and an abandoned, broken building.</p>`,
 	8: `<p>Don't bother looking; the anklet is covered up by Sparky's huge pant sleeves. This is not an excuse for poor drawing (although the drawing <i>is</i> poor). It's actually her pants.</p>`,
 	11: `<p>This page is drawn in Clip Studio Paint. Drawing comics in SAI was horrible. I will eventually redraw the previous ten pages with CSP too, but right now it's too early to be turning back.</p>`,
@@ -11,6 +12,10 @@ const authorsNotes = {
 	32: `<p>This page looks <i>really</i> lazy, doesn't it.</p>`,
 	34: `<p>My tablet broke down while drawing "party," and the new one is quite different from the old one and I'm still getting used to it. So I apologize if these pages look weird!</p>`,
 	37: `<p>Panel 2, Sparky is snorting lemonade with her nose /j.</p>`,
+	50: `<p>Whoa, hey, Tomato lost his ears in the second-to-last panel. Sorry about that.</p>`,
+	52: `<p>I can't help but say: Melody is so cool. So cool.</p><p>Also, the laziness is radiating off the last panel. Sorry, let's assume the others have mysteriously shuffled away so that we can only see Sparky and Tephra near the chair.</p>`,
+	55: `<p>Wow, Jerboa's arms are awfully long.</p>`,
+	57: `<p>Ah, yes, the three fundamental elements of the universe.</p>`,
 };
 // background stuff
 const BGStuff = {
@@ -54,7 +59,7 @@ window.onload = function() {
 		maxPageNum = last[3] + last[2] - 1;
 	}
 	if(maxPageNum < pagenum) flipPage(0);
-	changeThingInMiddle(pagenum);
+	changeNumberInMiddle(pagenum);
 	setImage(pagenum);
 	changeBG(pagenum);
 	addANotes(pagenum);
@@ -62,6 +67,7 @@ window.onload = function() {
 	doNoClicks();
 	saveButtons();
 	arrowNavigation();
+	typeNumberNavigation();
 }
 
 function arrowNavigation() {
@@ -108,11 +114,11 @@ function pageLinx() {
 function flipPage(num) {
 	if(num == 'next') num = Math.min(maxPageNum, pagenum + 1);
 	if(num == 'prev') num = Math.max(0, pagenum - 1);
-	if(num == pagenum || num > maxPageNum) return;
+	if(num == pagenum || !withinRange(num)) return;
 	window.scrollTo(0, 0);
 	window.history.pushState({}, null, `?page=${num}`);
 	document.title = `The Iron Ragdoll | Page ${num}`;
-	changeThingInMiddle(num);
+	changeNumberInMiddle(num);
 	preloadImg(num - 1);
 	preloadImg(num + 1);
 	setImage(num);
@@ -158,12 +164,30 @@ function addANotes(num) {
 	// document.getElementById('translation').children[0].innerHTML = translation[num];
 }
 
-function changeThingInMiddle(num) {
+function changeNumberInMiddle(num) {
 	// full fledge single letter variable mode but i dont care
-	let q = document.querySelectorAll('.pagenum')
+	let q = document.querySelectorAll('.pagenum');
 	for(let w of q) {
-		w.children[0].innerText = 'Page ' + num;
+		w.innerText = num;
 	};
+}
+
+function typeNumberNavigation() {
+	let q = document.querySelectorAll('.pagenum');
+	for(let w of q) {
+		w.addEventListener('keydown', e => {
+			if(e.key == 'Enter') {
+				e.preventDefault();
+				if(w.innerText.toLowerCase() == 'last') {
+					flipPage(maxPageNum);
+				} else {
+					let n = parseInt(w.innerText);
+					if(withinRange(n)) flipPage(n);
+					else w.innerText = pagenum;
+				}
+			}
+		});
+	}
 }
 
 function setImage(num) {
@@ -171,7 +195,10 @@ function setImage(num) {
 }
 
 function changeBG(num) {
-	while(!BGStuff[num]) num--;
+	while(!BGStuff[num]) {
+		num--;
+		if(num < 0 || Number.isNaN(num)) throw new YouAreSoStupidError('num is so wrong maybe its below 0 meybe its not a number');
+	}
 	document.body.style.backgroundImage = `url(bg/${BGStuff[num][0]}.png)`;
 	// let root = document.querySelector(':root');
 	// root.style.setProperty('--text', BGStuff[num][1]);
@@ -182,10 +209,24 @@ function getImgUrl(num) {
 	return String(Math.floor(num / 100) + '/' + num + '.png');
 }
 
+function withinRange(num) {
+	return num >= 0 && num <= maxPageNum;
+}
+
 // code from Grepper
 function clearEventListeners(el) {
 	clone = el.cloneNode(true);
 	el.parentNode.replaceChild(clone, el);
+}
+
+// code from Grepper
+class YouAreSoStupidError extends Error {
+	constructor(message) {
+		super(message);
+		this.name = this.constructor.name;
+		this.message = message;
+		Error.captureStackTrace(this, this.constructor);
+	}
 }
 
 function elt(type,props,...children){let dom=document.createElement(type);if(props)Object.assign(dom,props);for(let child of children){if(typeof child!="string")dom.appendChild(child);else dom.appendChild(document.createTextNode(child));}return(dom);}
