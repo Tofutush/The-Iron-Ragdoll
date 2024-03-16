9ad5387989f9b2146600afc655e0a01c8eadb8db
Tofutush <siroccoxxq@gmail.com>
2024-03-16 15:05:03 +0800

    update arrival

---
+++ authornotes.js
@@ -0,0 +1,18 @@
+// authors notes
+const authorsNotes = {
+	1: `<p>This is the start of The Iron Ragdoll! Thanks a lot for your willingness to give this a shot!</p>
+		<p>The Iron Ragdoll is also available on other platforms (see dropdown menu when hovering "Other" above). It would be so kind of you subscribe and leave comments!</p>`,
+	4: `<p>Sparky settles in her new home. She knows it's supposed to be bad, but somehow it's worse.</p>`,
+	8: `<p>Don't bother looking; the anklet is covered up by Sparky's huge pant sleeves. This is not an excuse for poor drawing (although the drawing <i>is</i> poor). It's actually her pants.</p>`,
+	11: `<p>This page is drawn in Clip Studio Paint. Drawing comics in SAI was horrible. I will eventually redraw the previous ten pages with CSP too, but right now it's too early to be turning back.</p>`,
+	17: `<p>When Cosmo asked Sparky whether she could remember names, she wasn't asking Sparky. She was asking <i>you</i>. Can you remember these characters? You will meet more of them.</p>`,
+	24: `<p>New day, new shitty problems!`,
+	28: `<p>Sparky in the bottom-left panel kinda looks like a gorilla. Sorry guys, I'm still working on it.</p>`,
+	32: `<p>This page looks <i>really</i> lazy, doesn't it.</p>`,
+	34: `<p>My tablet broke down while drawing "party," and the new one is quite different from the old one and I'm still getting used to it. So I apologize if these pages look weird!</p>`,
+	37: `<p>Panel 2, Sparky is snorting lemonade with her nose /j.</p>`,
+	50: `<p>Whoa, hey, Tomato lost his ears in the second-to-last panel. Sorry about that.</p>`,
+	52: `<p>I can't help but say: Melody is so cool. So cool.</p><p>Also, the laziness is radiating off the last panel. Sorry, let's assume the others have mysteriously shuffled away so that we can only see Sparky and Tephra near the chair.</p>`,
+	55: `<p>Wow, Jerboa's arms are awfully long.</p>`,
+	57: `<p>Ah, yes, the three fundamental elements of the universe.</p>`,
+};
