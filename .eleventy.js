df96f4292ec27c82e1bef21bf84d76ae77fb54a8
Tofutush <siroccoxxq@gmail.com>
2024-06-05 22:59:43 +0800

    pagination draft

--- .eleventy.js
+++ .eleventy.js
@@ -15,6 +15,21 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('page.js');
 	// fonts
 	eleventyConfig.addPassthroughCopy('stuff');
+	eleventyConfig.addFilter('getimgurl', function(num) {
+		num = parseInt(num);
+		return String('/' + Math.floor(num / 100) + '/' + num + '.png')
+	});
+	eleventyConfig.addShortcode('arrows', function(f, p, n, l, num) {
+		return `
+<div class="options">
+	<a ${p ? '' : 'class="noclick"'} href="${f}"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m11 17-5-5 5-5"/><path d="m18 17-5-5 5-5"/></svg></a>
+	<a ${p ? '' : 'class="noclick"'} href="${p}"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6"/></svg></a>
+	<p>Page <span class="pagenum" contenteditable="true">${num}</span></p>
+	<a ${n ? '' : 'class="noclick"'} href="${n}"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg></a>
+	<a ${n ? '' : 'class="noclick"'} href="${l}"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 17 5-5-5-5"/><path d="m13 17 5-5-5-5"/></svg></a>
+</div>
+		`;
+	});
 	return {
 		passthroughFileCopy: true
 	};
