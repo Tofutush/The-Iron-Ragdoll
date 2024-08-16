22ab28e8d2fba11e938ad961cd83a7b81a39ca92
Tofutush <siroccoxxq@gmail.com>
2024-08-16 12:56:51 +0800

    gallety tas

--- _data/gallery.js
+++ _data/gallery.js
@@ -1,4 +1,4 @@
 const gallery = require('./gallery imgs.json')
-module.exports = function() {
-    return gallery.sort((a, b) => a.name.localeCompare(b.name));
+module.exports = function() {	
+	return gallery.sort((a, b) => a.name.localeCompare(b.name));
 }
