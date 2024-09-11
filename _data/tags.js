865c7abd85884c6c5df8352406bd49dadacf3eae
Tofutush <siroccoxxq@gmail.com>
2024-09-11 14:25:41 +0800

    comment

--- _data/tags.js
+++ _data/tags.js
@@ -13,14 +13,14 @@ module.exports = function() {
 	return ch.sort((a, b) => a.localeCompare(b));
 }
 
-// https://stackoverflow.com/questions/3579486/sort-a-javascript-array-by-frequency-and-then-filter-repeats
-function sortByFrequency(array) {
-	var frequency = {};
-	array.forEach(function(value) { frequency[value] = 0; });
-	var uniques = array.filter(function(value) {
-		return ++frequency[value] == 1;
-	});
-	return uniques.sort(function(a, b) {
-		return frequency[b] - frequency[a];
-	});
-}
+// // https://stackoverflow.com/questions/3579486/sort-a-javascript-array-by-frequency-and-then-filter-repeats
+// function sortByFrequency(array) {
+// 	var frequency = {};
+// 	array.forEach(function(value) { frequency[value] = 0; });
+// 	var uniques = array.filter(function(value) {
+// 		return ++frequency[value] == 1;
+// 	});
+// 	return uniques.sort(function(a, b) {
+// 		return frequency[b] - frequency[a];
+// 	});
+// }
