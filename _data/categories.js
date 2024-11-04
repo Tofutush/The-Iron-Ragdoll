060170f6ac30d127901ac7b01cbbb1d77ce9a270
Tofutush <siroccoxxq@gmail.com>
2024-11-04 08:06:12 +0800

    add img

--- _data/categories.js
+++ _data/categories.js
@@ -1,12 +1,12 @@
-const chs = require('./characters.json');
-module.exports = function () {
-	let cats = [];
-	for (let ch of chs) {
-		if (!cats.includes(ch.cat)
-			&& ch.cat != 'main'
-			&& chs.filter(c => c.cat == ch.cat && c.hidden != true).length > 0) {
-			cats.push(ch.cat);
-		}
-	}
-	return cats;
-}
+// const chs = require('./characters.json');
+// module.exports = function () {
+// 	let cats = [];
+// 	for (let ch of chs) {
+// 		if (!cats.includes(ch.cat)
+// 			&& ch.cat != 'main'
+// 			&& chs.filter(c => c.cat == ch.cat && c.hidden != true).length > 0) {
+// 			cats.push(ch.cat);
+// 		}
+// 	}
+// 	return cats;
+// }
