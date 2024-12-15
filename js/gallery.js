fa917f8b0db0b6891f869fc4659cdc830c6a4961
Tofutush <Tofutush@users.noreply.github.com>
2024-12-15 09:02:48 +0000

    deploy: 8e0165e0360b551163de5ee048db34a4993cd4a1

--- js/gallery.js
+++ js/gallery.js
@@ -1,18 +1,19 @@
 (function () {
-	let divs = document.getElementById('gallery').children;
-	console.log(document.getElementById('gallery'));
-	for (let z = 0; z < divs.length; z++) {
-		let img = divs[z].querySelector('img');
-		img.addEventListener('click', e => {
-			console.log('clicked');
-			let modal = divs[z].querySelector('.modal');
-			let close = modal.querySelector('.close');
-			modal.style.display = 'flex';
-			document.body.style.overflow = 'hidden';
-			close.addEventListener('click', e => {
-				modal.style.display = 'none';
-				document.body.style.overflow = 'auto';
+	let galleries = document.querySelectorAll('.gallery');
+	for (gallery of galleries) {
+		let divs = gallery.children;
+		for (let z = 0; z < divs.length; z++) {
+			let img = divs[z].querySelector('img');
+			img.addEventListener('click', e => {
+				let modal = divs[z].querySelector('.modal');
+				let close = modal.querySelector('.close');
+				modal.style.display = 'flex';
+				document.body.style.overflow = 'hidden';
+				close.addEventListener('click', e => {
+					modal.style.display = 'none';
+					document.body.style.overflow = 'auto';
+				});
 			});
-		});
+		}
 	}
 })();
