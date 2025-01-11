e7ddbec2d02b31550d3d9cc10132b74efd1624df
Tofutush <xiexuanqisirocco@163.com>
2025-01-11 13:24:21 +0800

    wrking draft

--- js/generator.js
+++ js/generator.js
@@ -8,10 +8,15 @@ class Generator {
     generate() {
         let rolled = this.prompts[Math.floor(Math.random() * this.prompts.length)];
         let phs = rolled.match(/\%\w+\%/g);
-        console.log(phs);
-
         for (let z = 0; z < phs.length; z++) {
-
+            let id = phs[z].substring(1, phs[z].length - 1);
+            for (let x = 0; x < this.placeholders.length; x++) {
+                if (this.placeholders[x].id === id) {
+                    rolled = rolled.replace(phs[z], this.placeholders[x].roll());
+                    break;
+                }
+            }
         }
+        return rolled;
     }
 }
