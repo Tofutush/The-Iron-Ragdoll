42965cf7fa68aaa7903a09d10c531d53eae13497
Tofutush <Tofutush@users.noreply.github.com>
2025-02-19 07:05:10 +0000

    deploy: f55976b54ae23ce1f6eba3b171895d52a8a0d2c8

---
+++ characters/relationships/data.js
@@ -0,0 +1,716 @@
+const data = {
+    ch: [
+        
+            {
+                id: "Sparky",
+                color: "#0cf"
+            },
+        
+            {
+                id: "Ribbon",
+                color: "#e368d3"
+            },
+        
+            {
+                id: "Cosmo",
+                color: "#998cbd"
+            },
+        
+            {
+                id: "Tomato",
+                color: "#A10000"
+            },
+        
+            {
+                id: "Guillotine",
+                color: "#d0ae3a"
+            },
+        
+            {
+                id: "Tephra",
+                color: "#008282"
+            },
+        
+            {
+                id: "Melody",
+                color: "#426600"
+            },
+        
+            {
+                id: "Jerboa",
+                color: "#0867af"
+            },
+        
+            {
+                id: "Tundra",
+                color: "#800080"
+            },
+        
+            {
+                id: "Alaska",
+                color: "#6C0404"
+            },
+        
+            {
+                id: "Starry",
+                color: "#CC770F"
+            },
+        
+            {
+                id: "Dragonfly",
+                color: "#00adad"
+            },
+        
+            {
+                id: "Qibli",
+                color: "#440044"
+            },
+        
+            {
+                id: "Sky",
+                color: "#236ce1"
+            },
+        
+            {
+                id: "Firecrackers",
+                color: "#8214bd"
+            },
+        
+            {
+                id: "Myriad",
+                color: "#d01b84"
+            },
+        
+            {
+                id: "Amber",
+                color: "#e15f19"
+            },
+        
+            {
+                id: "Peacock",
+                color: "#20f"
+            },
+        
+            {
+                id: "Fantasy",
+                color: "#ce00af"
+            },
+        
+            {
+                id: "Ironheart",
+                color: "#69272b"
+            },
+        
+            {
+                id: "Willow",
+                color: "#199400"
+            },
+        
+            {
+                id: "Nightsun",
+                color: "#130b51"
+            },
+        
+            {
+                id: "Bramble",
+                color: "#E19D0E"
+            },
+        
+            {
+                id: "Goose",
+                color: "#D54269"
+            },
+        
+            {
+                id: "Pumpkin",
+                color: "#E3744D"
+            },
+        
+            {
+                id: "Halcyon",
+                color: "#008E9B"
+            },
+        
+            {
+                id: "Paperweight",
+                color: "#2e2e2e"
+            },
+        
+            {
+                id: "Gecko",
+                color: "#a40ca7"
+            },
+        
+            {
+                id: "Lark",
+                color: "#06B87B"
+            },
+        
+            {
+                id: "Pearl",
+                color: "#caf1f1"
+            },
+        
+            {
+                id: "Jackpot",
+                color: "#AC7C26"
+            },
+        
+            {
+                id: "Snake",
+                color: "#c30963"
+            },
+        
+            {
+                id: "Snowball",
+                color: "#D920D2"
+            },
+        
+            {
+                id: "Buttercup",
+                color: "#8c7503"
+            },
+        
+            {
+                id: "Beauty",
+                color: "#CE6C07"
+            },
+        
+            {
+                id: "Chastity",
+                color: "#C3BB4E"
+            },
+        
+            {
+                id: "Devotion",
+                color: "#017909"
+            },
+        
+            {
+                id: "Elegance",
+                color: "#1054ad"
+            },
+        
+            {
+                id: "Animus",
+                color: "#a10000"
+            },
+        
+            {
+                id: "Jaculus",
+                color: "#008bc7"
+            },
+        
+            {
+                id: "Koala",
+                color: "#7F8587"
+            },
+        
+            {
+                id: "Kangaroo",
+                color: "#A36D1F"
+            },
+        
+            {
+                id: "Robin",
+                color: "#a72b0c"
+            },
+        
+            {
+                id: "Frosting",
+                color: "#F676D2"
+            },
+        
+            {
+                id: "Candlelight",
+                color: "#cb1616"
+            },
+        
+            {
+                id: "Helsinki",
+                color: "#46119f"
+            },
+        
+            {
+                id: "Mooncake",
+                color: "#0b8445"
+            },
+        
+            {
+                id: "Fern",
+                color: "#089b19"
+            },
+        
+            {
+                id: "Sunny",
+                color: "#071cbb"
+            },
+        
+            {
+                id: "Vampire",
+                color: "#dd8736"
+            },
+        
+            {
+                id: "Moonbeam",
+                color: "#08d8e7"
+            },
+        
+            {
+                id: "Thunder",
+                color: "#DCCB09"
+            },
+        
+    ],
+    rel: [
+        
+            {
+                source: "Sparky",
+                target: "Ribbon",
+                rel1: "",
+                rel2: ""
+            },
+        
+            {
+                source: "Sparky",
+                target: "Qibli",
+                rel1: "friend",
+                rel2: "friend"
+            },
+        
+            {
+                source: "Sparky",
+                target: "Sky",
+                rel1: "friend",
+                rel2: "friend"
+            },
+        
+            {
+                source: "Sparky",
+                target: "Firecrackers",
+                rel1: "friend",
+                rel2: "friend"
+            },
+        
+            {
+                source: "Sparky",
+                target: "Amber",
+                rel1: "daughter",
+                rel2: "mother"
+            },
+        
+            {
+                source: "Sparky",
+                target: "Peacock",
+                rel1: "stepdaughter",
+                rel2: "stepfather"
+            },
+        
+            {
+                source: "Sparky",
+                target: "Fantasy",
+                rel1: "younger stepsister",
+                rel2: "older stepsister"
+            },
+        
+            {
+                source: "Sparky",
+                target: "Ironheart",
+                rel1: "younger stepsister",
+                rel2: "older stepbrother"
+            },
+        
+            {
+                source: "Sparky",
+                target: "Willow",
+                rel1: "older half-sister",
+                rel2: "younger half-sister"
+            },
+        
+            {
+                source: "Sparky",
+                target: "Nightsun",
+                rel1: "wife",
+                rel2: "husband"
+            },
+        
+            {
+                source: "Sparky",
+                target: "Bramble",
+                rel1: "aunt (legal guardian)",
+                rel2: "nephew"
+            },
+        
+            {
+                source: "Sparky",
+                target: "Pearl",
+                rel1: "",
+                rel2: ""
+            },
+        
+            {
+                source: "Sparky",
+                target: "Devotion",
+                rel1: "frienemy",
+                rel2: "frienemy"
+            },
+        
+            {
+                source: "Sparky",
+                target: "Elegance",
+                rel1: "friend",
+                rel2: "friend"
+            },
+        
+            {
+                source: "Sparky",
+                target: "Animus",
+                rel1: "friend / bodyguard",
+                rel2: "friend / principal"
+            },
+        
+            {
+                source: "Ribbon",
+                target: "Tundra",
+                rel1: "",
+                rel2: ""
+            },
+        
+            {
+                source: "Ribbon",
+                target: "Alaska",
+                rel1: "enemy",
+                rel2: "enemy"
+            },
+        
+            {
+                source: "Ribbon",
+                target: "Starry",
+                rel1: "caretaker",
+                rel2: "crush"
+            },
+        
+            {
+                source: "Cosmo",
+                target: "Tomato",
+                rel1: "enemy",
+                rel2: "enemy"
+            },
+        
+            {
+                source: "Tephra",
+                target: "Jerboa",
+                rel1: "boss",
+                rel2: "little pet"
+            },
+        
+            {
+                source: "Melody",
+                target: "Dragonfly",
+                rel1: "same person",
+                rel2: "same person"
+            },
+        
+            {
+                source: "Jerboa",
+                target: "Jaculus",
+                rel1: "younger brother",
+                rel2: "older sister"
+            },
+        
+            {
+                source: "Jerboa",
+                target: "Koala",
+                rel1: "son",
+                rel2: "mother"
+            },
+        
+            {
+                source: "Jerboa",
+                target: "Kangaroo",
+                rel1: "son",
+                rel2: "father"
+            },
+        
+            {
+                source: "Jerboa",
+                target: "Frosting",
+                rel1: "acquaintance",
+                rel2: "acquaintance"
+            },
+        
+            {
+                source: "Qibli",
+                target: "Sky",
+                rel1: "childhood friend",
+                rel2: "childhood friend"
+            },
+        
+            {
+                source: "Qibli",
+                target: "Firecrackers",
+                rel1: "friend",
+                rel2: "friend"
+            },
+        
+            {
+                source: "Sky",
+                target: "Firecrackers",
+                rel1: "",
+                rel2: "friend"
+            },
+        
+            {
+                source: "Sky",
+                target: "Goose",
+                rel1: "apprentice",
+                rel2: "shifu"
+            },
+        
+            {
+                source: "Amber",
+                target: "Peacock",
+                rel1: "wife",
+                rel2: "husband"
+            },
+        
+            {
+                source: "Amber",
+                target: "Fantasy",
+                rel1: "stepmother",
+                rel2: "stepdaughter"
+            },
+        
+            {
+                source: "Amber",
+                target: "Willow",
+                rel1: "mother",
+                rel2: "daughter"
+            },
+        
+            {
+                source: "Peacock",
+                target: "Fantasy",
+                rel1: "father",
+                rel2: "daughter"
+            },
+        
+            {
+                source: "Peacock",
+                target: "Ironheart",
+                rel1: "father",
+                rel2: "son"
+            },
+        
+            {
+                source: "Peacock",
+                target: "Willow",
+                rel1: "father",
+                rel2: "daughter"
+            },
+        
+            {
+                source: "Fantasy",
+                target: "Ironheart",
+                rel1: "older half-sister",
+                rel2: "younger half-brother"
+            },
+        
+            {
+                source: "Fantasy",
+                target: "Willow",
+                rel1: "older half-sister",
+                rel2: "younger half-sister"
+            },
+        
+            {
+                source: "Fantasy",
+                target: "Bramble",
+                rel1: "mother",
+                rel2: "son"
+            },
+        
+            {
+                source: "Ironheart",
+                target: "Willow",
+                rel1: "older half-brother",
+                rel2: "younger half-sister"
+            },
+        
+            {
+                source: "Nightsun",
+                target: "Bramble",
+                rel1: "stepfather",
+                rel2: "stepson"
+            },
+        
+            {
+                source: "Goose",
+                target: "Pumpkin",
+                rel1: "colleague / friend",
+                rel2: "colleague / friend"
+            },
+        
+            {
+                source: "Goose",
+                target: "Paperweight",
+                rel1: "apprentice",
+                rel2: "shifu"
+            },
+        
+            {
+                source: "Pumpkin",
+                target: "Halcyon",
+                rel1: "apprentice",
+                rel2: "shifu"
+            },
+        
+            {
+                source: "Pumpkin",
+                target: "Paperweight",
+                rel1: "",
+                rel2: "superior"
+            },
+        
+            {
+                source: "Pumpkin",
+                target: "Candlelight",
+                rel1: "boyfriend",
+                rel2: "boyfriend"
+            },
+        
+            {
+                source: "Pumpkin",
+                target: "Helsinki",
+                rel1: "distant relative",
+                rel2: "distant relative"
+            },
+        
+            {
+                source: "Pearl",
+                target: "Snake",
+                rel1: "friend",
+                rel2: "friend"
+            },
+        
+            {
+                source: "Jackpot",
+                target: "Snake",
+                rel1: "father",
+                rel2: "son"
+            },
+        
+            {
+                source: "Beauty",
+                target: "Chastity",
+                rel1: "older sister",
+                rel2: "younger sister"
+            },
+        
+            {
+                source: "Beauty",
+                target: "Devotion",
+                rel1: "older sister",
+                rel2: "younger sister"
+            },
+        
+            {
+                source: "Beauty",
+                target: "Elegance",
+                rel1: "older sister",
+                rel2: "younger sister"
+            },
+        
+            {
+                source: "Beauty",
+                target: "Animus",
+                rel1: "older sister",
+                rel2: "younger brother"
+            },
+        
+            {
+                source: "Chastity",
+                target: "Devotion",
+                rel1: "older sister",
+                rel2: "younger sister"
+            },
+        
+            {
+                source: "Chastity",
+                target: "Elegance",
+                rel1: "older sister",
+                rel2: "younger sister"
+            },
+        
+            {
+                source: "Chastity",
+                target: "Animus",
+                rel1: "older sister",
+                rel2: "younger brother"
+            },
+        
+            {
+                source: "Devotion",
+                target: "Elegance",
+                rel1: "older sister",
+                rel2: "younger sister"
+            },
+        
+            {
+                source: "Devotion",
+                target: "Animus",
+                rel1: "older sister",
+                rel2: "younger brother"
+            },
+        
+            {
+                source: "Elegance",
+                target: "Animus",
+                rel1: "older sister",
+                rel2: "younger brother"
+            },
+        
+            {
+                source: "Jaculus",
+                target: "Koala",
+                rel1: "daughter",
+                rel2: "mother"
+            },
+        
+            {
+                source: "Jaculus",
+                target: "Kangaroo",
+                rel1: "daughter",
+                rel2: "father"
+            },
+        
+            {
+                source: "Jaculus",
+                target: "Frosting",
+                rel1: "BFF",
+                rel2: "BFF"
+            },
+        
+            {
+                source: "Koala",
+                target: "Kangaroo",
+                rel1: "wife",
+                rel2: "husband"
+            },
+        
+            {
+                source: "Robin",
+                target: "Frosting",
+                rel1: "father",
+                rel2: "daughter"
+            },
+        
+            {
+                source: "Candlelight",
+                target: "Mooncake",
+                rel1: "childhood friend",
+                rel2: "childhood friend"
+            },
+        
+    ]
+};
