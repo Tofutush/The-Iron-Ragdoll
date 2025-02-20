fbd96f94f4a526f016ca76686cea2e0ecbe187e3
Tofutush <xiexuanqisirocco@163.com>
2025-02-20 09:51:11 +0800

    img  url

--- .eleventy.js
+++ .eleventy.js
@@ -174,12 +174,7 @@ module.exports = function (eleventyConfig) {
 		let src = getImgSrc(path, name, fallback);
 		let dimensions = imageSize(src);
 		let format = (dimensions.width > 16383 || dimensions.height > 16383) ? 'png' : 'webp';
-		let metadata = await Image(src, {
-			widths: [size],
-			formats: [format],
-			urlPath: '/img/' + path,
-			outputDir: './_site/img/' + path
-		});
+		let metadata = await getImg(src, size, format, path);
 		let imageAttributes = {
 			alt,
 			title: alt,
@@ -191,12 +186,7 @@ module.exports = function (eleventyConfig) {
 	});
 	eleventyConfig.addShortcode('figure', async function (path, name, size, alt, caption, className) {
 		let src = (existsSync('img/' + path + name)) ? 'img/' + path + name : "img/bg/placeholder.png";
-		let metadata = await Image(src, {
-			widths: [size],
-			formats: ['webp'],
-			urlPath: '/img/' + path,
-			outputDir: './_site/img/' + path
-		});
+		let metadata = await getImg(src, size, 'webp', path);
 		let imageAttributes = {
 			alt,
 			title: alt,
@@ -208,11 +198,21 @@ module.exports = function (eleventyConfig) {
 	});
 	eleventyConfig.addShortcode('imageUrl', async function (path, name, size, fallback) {
 		let src = getImgSrc(path, name, fallback);
+		let metadata = await getImg(src, size, 'webp', path);
+		return metadata.webp[0].url;
 	});
 	function getImgSrc(path, name, fallback) {
 		if (existsSync('img/' + path + name)) return 'img/' + path + name;
 		if (fallback && existsSync('img/' + path + fallback)) return 'img/' + path + fallback;
 		return 'img/bg/placeholder.png';
+	}
+	async function getImg(src, size, format, path) {
+		return await Image(src, {
+			widths: [size],
+			formats: [format],
+			urlPath: '/img/' + path,
+			outputDir: './_site/img/' + path
+		});
 	}
 	eleventyConfig.addTransform("htmlmin", async function (content) {
 		if ((this.page.outputPath || "").endsWith(".html")) {
