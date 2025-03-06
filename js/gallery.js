59f028ee722fc110ce25b068eed419598b1e59ba
Tofutush <xiexuanqisirocco@163.com>
2025-03-06 10:21:39 +0800

    popup redo

--- js/gallery.js
+++ js/gallery.js
@@ -1,19 +1,16 @@
 (function () {
-	let galleries = document.querySelectorAll('.gallery');
-	for (gallery of galleries) {
-		let divs = gallery.children;
-		for (let z = 0; z < divs.length; z++) {
-			let img = divs[z].querySelector('img');
-			img.addEventListener('click', e => {
-				let modal = divs[z].querySelector('.modal');
-				let close = modal.querySelector('.close');
-				modal.style.display = 'flex';
-				document.body.style.overflow = 'hidden';
-				close.addEventListener('click', e => {
-					modal.style.display = 'none';
-					document.body.style.overflow = 'auto';
-				});
-			});
-		}
+	let images = document.querySelectorAll('.img');
+	for (let z = 0; z < images.length; z++) {
+		let img = images[z].querySelector('img');
+		let modal = images[z].querySelector('.modal');
+		let close = modal.querySelector('.close');
+		img.addEventListener('click', e => {
+			modal.style.display = 'flex';
+			document.body.style.overflow = 'hidden';
+		});
+		close.addEventListener('click', e => {
+			modal.style.display = 'none';
+			document.body.style.overflow = 'auto';
+		});
 	}
 })();
