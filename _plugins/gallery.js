48e1886e75ab0a11bd625cc5ca32e9e80f65927d
Tofutush <xiexuanqisirocco@163.com>
2025-03-13 09:56:39 +0800

    sort order

--- _plugins/gallery.js
+++ _plugins/gallery.js
@@ -12,7 +12,11 @@ function galleryPlugin(eleventyConfig) {
         return arr.filter(a => !a.author);
     });
     eleventyConfig.addFilter('sortGalleryByChCount', function (arr) {
-        return arr.sort((a, b) => a.ch?.length - b.ch?.length);
+        return arr.sort((a, b) => a.ch?.length - b.ch?.length).sort((a, b) => {
+            if (!a.author && b.author) return -1;
+            if (a.author && !b.author) return 1;
+            return 0;
+        });
     });
 }
 module.exports = galleryPlugin;
