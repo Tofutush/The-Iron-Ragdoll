86f679be06b699969c54a1f6c16a609021db3446
Tofutush <xiexuanqisirocco@163.com>
2025-03-13 09:56:39 +0800

    sort order

--- _data/gallery.js
+++ _data/gallery.js
@@ -1,4 +1,8 @@
 const gallery = require('./gallery imgs.json')
 module.exports = function () {
-	return gallery.sort((a, b) => a.name.localeCompare(b.name)).sort((a, b) => b.date.localeCompare(a.date));
+	return gallery.sort((a, b) => a.name.localeCompare(b.name)).sort((a, b) => {
+		if (!a.author && b.author) return -1;
+		if (a.author && !b.author) return 1;
+		return 0;
+	});
 }
