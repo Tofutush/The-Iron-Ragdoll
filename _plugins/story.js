5b0db19ce95c33aedd23d5baa5c2b32dbbf18297
Tofutush <xiexuanqisirocco@163.com>
2025-03-14 10:38:07 +0800

    story tags colledction

--- _plugins/story.js
+++ _plugins/story.js
@@ -2,6 +2,16 @@ function storyPlugin(eleventyConfig) {
     eleventyConfig.addCollection("stories", collection =>
         collection.getFilteredByGlob('tir/stories/*.md').sort((a, b) => a.data.order - b.data.order)
     );
+    eleventyConfig.addCollection("storyTags", collection => {
+        let stories = collection.getFilteredByGlob('tir/stories/*.md').sort((a, b) => a.data.order - b.data.order);
+        let tags = [];
+        for (let z = 0; z < stories.length; z++) {
+            if (!stories[z].data.myTags) continue;
+            for (let a of stories[z].data.myTags) tags.push(a);
+        }
+        tags = [...new Set(tags)];
+        return tags;
+    });
     eleventyConfig.addFilter('getStoryTags', function (arr) {
         let tags = [];
         for (let z = 0; z < arr.length; z++) {
