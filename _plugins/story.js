4ee976b6d961debdcf463f76709ea734eb8f58fd
Tofutush <xiexuanqisirocco@163.com>
2025-03-14 11:00:19 +0800

    story filter

--- _plugins/story.js
+++ _plugins/story.js
@@ -12,17 +12,8 @@ function storyPlugin(eleventyConfig) {
         tags = [...new Set(tags)];
         return tags;
     });
-    eleventyConfig.addFilter('getStoryTags', function (arr) {
-        let tags = [];
-        for (let z = 0; z < arr.length; z++) {
-            if (!arr[z].data.myTags) continue;
-            for (let a of arr[z].data.myTags) tags.push(a);
-        }
-        tags = [...new Set(tags)];
-        return tags;
-    });
     eleventyConfig.addFilter('filterStoriesByTag', function (arr, tag) {
-        return arr.filter(a => a.data.myTags === tag);
+        return arr.filter(a => a.data.myTags?.includes(tag));
     });
 }
 module.exports = storyPlugin;
