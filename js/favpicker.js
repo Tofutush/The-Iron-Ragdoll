13017b3f5da8a4240e02672fa6fc640ddbeb8521
Tofutush <xiexuanqisirocco@163.com>
2025-04-06 02:25:03 +0800

    finished, i think

--- js/favpicker.js
+++ js/favpicker.js
@@ -18,15 +18,16 @@ let idx = [];
 
 window.onload = function () {
     randTwo();
-    one.btn.addEventListener('click', choose(idx[1]));
-    two.btn.addEventListener('click', choose(idx[1]));
+    one.btn.addEventListener('click', eliminate(true));
+    two.btn.addEventListener('click', eliminate(false));
     setBoth(idx);
 }
 
-function choose(idx) {
+function eliminate(which) {
     return e => {
-        oldData = [...data];
-        data.splice(idx, 1);
+        if (which === true) data.splice(idx[1], 1); // eliminate two
+        else data.splice(idx[0], 1); //eliminate one
+        if (data.length <= 1) displayResult();
         randTwo();
         setBoth();
     }
@@ -44,11 +45,25 @@ function setDisplay(obj, id) {
     obj.p.innerText = data[id].desc;
 }
 
+function displayResult() {
+    document.querySelector('.picker').style.display = 'none';
+    const result = document.querySelector('.result');
+    result.querySelector('img').src = data[0].img;
+    result.style.display = 'block';
+    const link = result.querySelector('.fav');
+    link.innerText = data[0].name;
+    link.href = `/characters/${data[0].name.toLowerCase()}/`;
+    throw new Error('game ended');
+}
+
 function randTwo() {
+    if (data.length === 2) return [0, 1];
     let idx1 = Math.floor(Math.random() * data.length);
     let idx2 = Math.floor(Math.random() * data.length);
     do {
         idx2 = Math.floor(Math.random() * data.length);
+        console.log(idx1, idx2);
+
     } while (idx1 === idx2);
     idx = [idx1, idx2];
 }
