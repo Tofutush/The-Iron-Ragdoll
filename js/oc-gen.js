a1995606f1ea5c202dfdb1f70e7488c56c4b23dc
Tofutush <xiexuanqisirocco@163.com>
2025-04-10 16:42:06 +0800

    basics

--- js/oc-gen.js
+++ js/oc-gen.js
@@ -1,20 +1,20 @@
 let basicGenerator = new Generator(
     [
         new GeneratorPlaceholder('count', [
-            'Bauhinia',
-            'Fanton',
-            'Atlasia',
-            'Westavia',
-            'Moreland',
-            'Sylvandia'
+            '<a href="/world/bauhinia/">Bauhinia</a>',
+            '<a href="/world/fanton/">Fanton</a>',
+            '<a href="/world/atlasia/">Atlasia</a>',
+            '<a href="/world/westavia/">Westavia</a>',
+            '<a href="/world/moreland/">Moreland</a>',
+            '<a href="/world/sylvandia/">Sylvandia</a>'
         ]),
         new GeneratorPlaceholder('nat', [
-            'a Bauhinian',
-            'a Fantoni',
-            'an Atlasian',
-            'a Westavian',
-            'a Moose',
-            'a Sylvandian'
+            'a <a href="/world/bauhinia/">Bauhinian</a>',
+            'a <a href="/world/fanton/">Fantoni</a>',
+            'an <a href="/world/atlasia/">Atlasian</a>',
+            'a <a href="/world/westavia/">Westavian</a>',
+            'a <a href="/world/moreland/">Moose</a>',
+            'a <a href="/world/sylvandia/">Sylvandian</a>'
         ]),
         new GeneratorPlaceholder('race', [
             'aurian',
@@ -22,9 +22,35 @@ let basicGenerator = new Generator(
             'bicorn'
         ]),
         new GeneratorPlaceholder('adj', corporaAdjectives),
+        new GeneratorPlaceholder('ch', [
+            ...characters,
+            'someone in the <a href="/world/bauhinia/mss/">MSS</a>',
+            'someone in <a href="/world/bauhinia/starcorp/">Starcorp</a>',
+            'someone in the <a href="/world/moreland/guardian-moose/mic/">MIC</a>',
+            'someone in the <a href="/world/westavia/spf/">SPF</a>'
+        ]),
+        new GeneratorPlaceholder('rel', [
+            'are in love with',
+            'are great friends with',
+            'are friends with',
+            'are the arch nemesis of',
+            'love',
+            'hate',
+            'would like to kill',
+            'want a kid that\'s just like',
+            'want a parent that\'s just like',
+            'have a crush on'
+        ])
     ],
     [
-        'They are %nat% %race% living in %count%. They are %adj|1-3%.'
+        'They are %nat% %race%.',
+        'They are %nat% %race%. They are %adj|1-3%.',
+        'They are %nat% %race%. They are %adj|1-3%. They %rel% %ch%.',
+        'They are %nat% %race%. They are %adj|1-3%. They %rel% %ch%.',
+        'They are %nat% %race%.',
+        'They are %nat% %race% living in %count%.',
+        'They are %nat% %race% living in %count%. They are %adj|1-3%.',
+        'They are %nat% %race% living in %count%. They are %adj|1-3%. They %rel% %ch%.'
     ]
 );
 let soulGenerator = new Generator(
@@ -64,4 +90,8 @@ function generate() {
     let colorSpan = document.querySelector('.color');
     colorSpan.innerHTML = color.toUpperCase();
     colorSpan.style.color = color;
+
+    let soul = soulGenerator.generate();
+    let soulP = document.querySelector('.soul');
+    soulP.innerHTML = soul;
 }
