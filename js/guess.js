42d862dbe6c65a35097b364a1bd72d5c17e6733e
Tofutush <xiexuanqisirocco@163.com>
2025-05-16 15:40:57 +0800

    more scripting ahh

--- js/guess.js
+++ js/guess.js
@@ -1,18 +1,34 @@
+let lastQuestion;
+let rounds = 0;
+let lives = 3;
+
 function startGame() {
     document.getElementById('new-game').style.display = 'none';
     document.getElementById('game').style.display = 'block';
-    let rounds = 0;
-    let lives = 3;
-    while (lives > 0) {
-        let q = getQuestion();
-        console.log(q);
-
-        displayQuestion(q);
-    }
+    displayQuestion();
 }
-function displayQuestion(q) {
+function displayQuestion() {
+    let q = getQuestion();
+    let options = getOptions(q, rounds);
     document.getElementById('ch').src = imgs[q.ch];
     document.getElementById('rel').innerHTML = q.rel;
+    let optionElt = document.getElementById('options');
+    optionElt.innerHTML = '';
+    for (let z = 0; z < options.length; z++) {
+        let img = elt('img', { src: imgs[options[z]] });
+        optionElt.appendChild(img);
+    }
+}
+function getOptions(q, rounds) {
+    let options = [q.answer];
+    for (let z = 0; z < rounds % 10 + 1; z++) {
+        let falseOption;
+        do {
+            falseOption = chs[randomInRange(0, chs.length)]
+        } while (options.includes(falseOption))
+        options.push(falseOption);
+    }
+    return options;
 }
 function getQuestion() {
     let question;
@@ -28,9 +44,9 @@ function getQuestion() {
             rel: rel[0][1],
             answer: rel[0][0]
         };
-    } while (question && lastQuestion &&
+    } while (lastQuestion !== undefined &&
         (lastQuestion.ch !== question.ch ||
-            lastQuestion.answer !== question.answer)
+            lastQuestion.rel !== question.rel)
     );
     return question;
 }
@@ -39,4 +55,14 @@ function randomInRange(a, b) {
     a = parseInt(a);
     b = parseInt(b);
     return Math.floor(Math.random() * (b - a + 1)) + a;
+}
+function elt(name, attrs, ...children) {
+    let dom = document.createElement(name);
+    for (let attr of Object.keys(attrs)) {
+        dom.setAttribute(attr, attrs[attr]);
+    }
+    for (let child of children) {
+        dom.appendChild(child);
+    }
+    return dom;
 }
