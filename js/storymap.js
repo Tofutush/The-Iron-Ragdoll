9a79ead1978265268017cbe93facfa4bf60f4183
Tofutush <Tofutush@users.noreply.github.com>
2025-11-28 05:16:24 +0000

    deploy: 582791f22bb62a1dbbd618ea5c2392a762cc160d

---
+++ js/storymap.js
@@ -0,0 +1,117 @@
+import * as d3 from "https://cdn.skypack.dev/d3@7.8.4";
+import * as d3dag from "https://cdn.skypack.dev/d3-dag@1.0.0-1";
+
+// ----- //
+// Setup //
+// ----- //
+
+// create our builder and turn the raw data into a graph
+const builder = d3dag.graphStratify();
+const graph = builder(data);
+
+// -------------- //
+// Compute Layout //
+// -------------- //
+
+// set the layout functions
+const nodeRadius = 20;
+// use this to render our edges
+const line = d3.line().curve(d3.curveMonotoneY);
+// here's the layout operator, uncomment some of the settings
+const layout = d3dag
+    .sugiyama()
+    //.layering(d3dag.layeringLongestPath())
+    //.decross(d3dag.decrossOpt())
+    //.coord(d3dag.coordGreedy())
+    //.coord(d3dag.coordQuad())
+    .nodeSize([nodeRadius * 2, nodeRadius * 2])
+    .gap([nodeRadius, nodeRadius])
+
+// actually perform the layout and get the final size
+const { width, height } = layout(graph);
+
+// --------- //
+// Rendering //
+// --------- //
+
+// global
+const svg = d3
+    .select("#svg")
+    // pad a little for link thickness
+    .style("width", width + 4)
+    .style("height", height + 4);
+const trans = svg.transition().duration(750);
+
+// nodes
+svg
+    .select("#nodes")
+    .selectAll("g")
+    .data(graph.nodes())
+    .join((enter) =>
+        enter
+            .append("g")
+            .attr("transform", ({ x, y }) => `translate(${x}, ${y})`)
+            .attr("opacity", 0)
+            .call((enter) => {
+                enter
+                    .append("a")
+                    .attr("href", n => n.data.url.replace("/stories/", "../"))
+                    .append("title").text(n => n.data.id);
+                enter
+                    .selectAll("a")
+                    .append("circle")
+                    .attr("r", nodeRadius)
+                    .attr("fill", "var(--c)");
+                enter.transition(trans).attr("opacity", 1);
+            })
+    );
+
+// link paths
+svg
+    .select("#links")
+    .selectAll("path")
+    .data(graph.links())
+    .join((enter) =>
+        enter
+            .append("path")
+            .attr("d", ({ points }) => line(points))
+            .attr("stroke-width", 3)
+            .attr("stroke", "var(--c)")
+            .attr("fill", "none")
+            .attr("opacity", 0)
+            .call((enter) => enter.transition(trans).attr("opacity", 1))
+    );
+
+function wrap(text, width) {
+    text.each(function () {
+        const text = d3.select(this);
+        const words = text.text().split(/\s+/).reverse();
+        let line = [];
+        let lineNumber = 0;
+        const lineHeight = 1.5; // em
+        const y = text.attr("y") || 0;
+        const dy = 0;
+
+        let tspan = text.text(null)
+            .append("tspan")
+            .attr("x", 0)
+            .attr("y", y)
+            .attr("dy", dy + "em");
+
+        let word;
+        while (word = words.pop()) {
+            line.push(word);
+            tspan.text(line.join(" "));
+            if (tspan.node().getComputedTextLength() > width) {
+                line.pop();
+                tspan.text(line.join(" "));
+                line = [word];
+                tspan = text.append("tspan")
+                    .attr("x", 0)
+                    .attr("y", y)
+                    .attr("dy", ++lineNumber * lineHeight + dy + "em")
+                    .text(word);
+            }
+        }
+    });
+}
---
+++ stories/map/data.js
@@ -0,0 +1,367 @@
+const data = [
+
+    {
+        id: "Coastline Middle School Mysteries",
+        parentIds: [
+            
+                "Peacock's Backstory",
+            
+        ],
+        url: "/stories/cms-mysteries/"
+    },
+
+    {
+        id: "Sparky Turns into a Dragon",
+        parentIds: [
+            
+                "Coastline Middle School Mysteries",
+            
+        ],
+        url: "/stories/sparky-turns-into-a-dragon/"
+    },
+
+    {
+        id: "A Nuke From Moreland",
+        parentIds: [
+            
+                "Sparky Turns into a Dragon",
+            
+                "Pumpkin's Backstory",
+            
+                "Goose's Backstory",
+            
+                "Halcyon's Backstory",
+            
+                "Paperweight's Backstory",
+            
+        ],
+        url: "/stories/a-nuke-from-moreland/"
+    },
+
+    {
+        id: "New Year 2032",
+        parentIds: [
+            
+                "A Nuke From Moreland",
+            
+        ],
+        url: "/stories/new-year-2032/"
+    },
+
+    {
+        id: "Pearl's Journey Home",
+        parentIds: [
+            
+                "New Year 2032",
+            
+        ],
+        url: "/stories/pearls-journey-home/"
+    },
+
+    {
+        id: "Pearl's Journey Home - Fanton",
+        parentIds: [
+            
+                "Pearl's Journey Home",
+            
+        ],
+        url: "/stories/pearls-journey-home-fanton/"
+    },
+
+    {
+        id: "Pearl's Journey Home - Atlasia",
+        parentIds: [
+            
+                "Pearl's Journey Home - Fanton",
+            
+                "Jackpot's Backstory",
+            
+        ],
+        url: "/stories/pearls-journey-home-atlasia/"
+    },
+
+    {
+        id: "Pearl's Journey Home - Moreland",
+        parentIds: [
+            
+                "Pearl's Journey Home - Atlasia",
+            
+        ],
+        url: "/stories/pearls-journey-home-moreland/"
+    },
+
+    {
+        id: "Pearl's Journey Home - Sylvandia",
+        parentIds: [
+            
+                "Pearl's Journey Home - Moreland",
+            
+        ],
+        url: "/stories/pearls-journey-home-sylvandia/"
+    },
+
+    {
+        id: "The Magic Academy",
+        parentIds: [
+            
+                "Coastline Middle School Mysteries",
+            
+        ],
+        url: "/stories/the-magic-academy/"
+    },
+
+    {
+        id: "The Yacht Trip",
+        parentIds: [
+            
+                "Pearl's Journey Home - Sylvandia",
+            
+        ],
+        url: "/stories/the-yacht-trip/"
+    },
+
+    {
+        id: "Elegance Kidnapped",
+        parentIds: [
+            
+                "The Yacht Trip",
+            
+        ],
+        url: "/stories/elegance-kidnapped/"
+    },
+
+    {
+        id: "Paperweight's Backstory",
+        parentIds: [
+            
+        ],
+        url: "/stories/paperweight-backstory/"
+    },
+
+    {
+        id: "Paperweight's Roadtrip",
+        parentIds: [
+            
+                "A Nuke From Moreland",
+            
+        ],
+        url: "/stories/paperweights-roadtrip/"
+    },
+
+    {
+        id: "Sunflowers in the Dark (working title)",
+        parentIds: [
+            
+                "The Occupation",
+            
+        ],
+        url: "/stories/sunflowers-in-the-dark/"
+    },
+
+    {
+        id: "Candlelight Back Home",
+        parentIds: [
+            
+                "Sunflowers in the Dark (working title)",
+            
+                "A Nuke From Moreland",
+            
+        ],
+        url: "/stories/candlelight-back-home/"
+    },
+
+    {
+        id: "Qibli on the Run",
+        parentIds: [
+            
+                "Pearl's Journey Home - Sylvandia",
+            
+                "The Magic Academy",
+            
+        ],
+        url: "/stories/qibli-on-the-run/"
+    },
+
+    {
+        id: "Project Boomerang",
+        parentIds: [
+            
+        ],
+        url: "/stories/project-boomerang/"
+    },
+
+    {
+        id: "The Hunt for Tundra (working title)",
+        parentIds: [
+            
+                "Project Boomerang",
+            
+        ],
+        url: "/stories/the-hunt-for-tundra/"
+    },
+
+    {
+        id: "Project Kismet",
+        parentIds: [
+            
+                "The Hunt for Tundra (working title)",
+            
+        ],
+        url: "/stories/project-kismet/"
+    },
+
+    {
+        id: "Poker Spies",
+        parentIds: [
+            
+                "Pearl's Journey Home - Sylvandia",
+            
+        ],
+        url: "/stories/poker-spies/"
+    },
+
+    {
+        id: "Spy School",
+        parentIds: [
+            
+                "Project Boomerang",
+            
+        ],
+        url: "/stories/spy-school/"
+    },
+
+    {
+        id: "Poker Party",
+        parentIds: [
+            
+                "Spy School",
+            
+        ],
+        url: "/stories/poker-party/"
+    },
+
+    {
+        id: "Family of Secrets",
+        parentIds: [
+            
+                "Poker Party",
+            
+        ],
+        url: "/stories/family-of-secrets/"
+    },
+
+    {
+        id: "Graduation",
+        parentIds: [
+            
+                "Family of Secrets",
+            
+        ],
+        url: "/stories/graduation/"
+    },
+
+    {
+        id: "One Man Short",
+        parentIds: [
+            
+        ],
+        url: "/stories/one-man-short/"
+    },
+
+    {
+        id: "Sunflower 2 (DEFINITELY working title)",
+        parentIds: [
+            
+                "Sunflowers in the Dark (working title)",
+            
+                "Qibli on the Run",
+            
+        ],
+        url: "/stories/sunflower-2/"
+    },
+
+    {
+        id: "Buttercup's Backstory",
+        parentIds: [
+            
+        ],
+        url: "/stories/buttercup-backstory/"
+    },
+
+    {
+        id: "Goose's Backstory",
+        parentIds: [
+            
+        ],
+        url: "/stories/goose-backstory/"
+    },
+
+    {
+        id: "Halcyon's Backstory",
+        parentIds: [
+            
+        ],
+        url: "/stories/halcyon-backstory/"
+    },
+
+    {
+        id: "Jackpot's Backstory",
+        parentIds: [
+            
+        ],
+        url: "/stories/jackpot-backstory/"
+    },
+
+    {
+        id: "Peacock's Backstory",
+        parentIds: [
+            
+        ],
+        url: "/stories/peacock-backstory/"
+    },
+
+    {
+        id: "Pumpkin's Backstory",
+        parentIds: [
+            
+                "Nameless Village Backstory",
+            
+        ],
+        url: "/stories/pumpkin-backstory/"
+    },
+
+    {
+        id: "Nameless Village Backstory",
+        parentIds: [
+            
+        ],
+        url: "/stories/nameless-village-backstory/"
+    },
+
+    {
+        id: "The Occupation",
+        parentIds: [
+            
+                "Nameless Village Backstory",
+            
+        ],
+        url: "/stories/the-occupation/"
+    },
+
+    {
+        id: "The Absolute End to TIR as We Know It",
+        parentIds: [
+            
+        ],
+        url: "/stories/the-absolute-end-to-tir-as-we-know-it/"
+    },
+
+    {
+        id: "Oneshots",
+        parentIds: [
+            
+        ],
+        url: "/stories/oneshots/"
+    },
+
+]
