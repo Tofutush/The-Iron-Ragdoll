2d4d2e6dd8c0d1b3a96bdba6a4849e41017dead8
Tofutush <xiexuanqisirocco@163.com>
2025-12-25 15:56:23 -0500

    save

--- js/quiz.js
+++ js/quiz.js
@@ -3,12 +3,15 @@ class Quiz {
 		this.div = div;
 		this.desc = quizContent.desc;
 		this.questions = quizContent.questions;
-		this.answers = quizContent.answers;
+		this.results = quizContent.results;
 		this.randomize = quizContent.randomize;
+
+		this.responses = [];
 	}
 
 	init() {
 		this.div.appendChild(elt('p', {}, this.desc));
+		this.div.appendChild(elt('p', { className: 'graybox' }, 'Select an answer for the options below. You can also leave questions empty. Some questions may be multi-select, and will be marked as such.'));
 		// questions
 		let bigQDiv = elt('div');
 		for (let z = 0; z < this.questions.length; z++) {
@@ -20,7 +23,7 @@ class Quiz {
 			if (this.randomize) shuffle(q.answers);
 			for (let x = 0; x < q.answers.length; x++) {
 				let a = q.answers[x];
-				let inputDiv = elt('input', { name: 'q' + z, type: 'radio' });
+				let inputDiv = elt('input', { name: 'q' + z, type: 'radio', onchange: e => this.setAnswer(z, a.response) });
 				let labelDiv = elt('label', {}, inputDiv, a.text);
 				let liDiv = elt('li', {}, labelDiv);
 				ulDiv.appendChild(liDiv);
@@ -31,10 +34,24 @@ class Quiz {
 		this.div.appendChild(bigQDiv);
 		// submit
 		this.div.appendChild(elt('hr'));
-		this.div.appendChild(elt('button', { style: 'display: block; margin: auto', onclick: this.submit }, 'Submit!'));
+		this.div.appendChild(elt('button', { style: 'display: block; margin: auto', onclick: () => this.submit() }, 'Submit!'));
 	}
+
+	setAnswer(qIdx, personalities) {
+		this.responses[qIdx] = personalities;
+	}
+
 	submit() {
-		console.log('submitted');
+		let result = [];
+		for (let idx in Object.keys(this.results)) result[idx] = [Object.keys(this.results)[idx], 0];
+		for (let z = 0; z < this.responses.length; z++) {
+			if (this.responses[z]) {
+				for (let personality of Object.keys(this.responses[z])) {
+					result.find(r => r[0] === personality)[1] += this.responses[z][personality];
+				}
+			}
+		}
+		console.log(result);
 
 	}
 }
