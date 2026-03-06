b2ead4fce0c4b284b96cc2f4cfbe3b8c54db7058
Tofutush <Tofutush@users.noreply.github.com>
2026-03-06 20:14:22 +0000

    deploy: 6752f63b6feb1d2e0b147d6e60fe099137972bdd

--- js/gallery.js
+++ js/gallery.js
@@ -1,18 +1,22 @@
 (function () {
+	let viewing = false;
 	let images = document.querySelectorAll('.img');
 	for (let z = 0; z < images.length; z++) {
-		let img = images[z].querySelector('img');
+		let thumb = images[z].querySelector('.thumb');
 		let modal = images[z].querySelector('.modal');
 		let close = modal.querySelector('.close');
 		let prev = modal.querySelector('.prev');
 		let next = modal.querySelector('.next');
-		img.addEventListener('click', e => {
+		thumb.addEventListener('click', e => {
+			if (viewing) return;
 			modal.style.display = 'flex';
 			document.body.style.overflow = 'hidden';
+			viewing = true;
 		});
 		close.addEventListener('click', e => {
 			modal.style.display = 'none';
 			document.body.style.overflow = 'auto';
+			viewing = false;
 		});
 		if (z === 0) prev.classList.add('noclick');
 		else prev.addEventListener('click', e => {
