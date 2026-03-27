e9e099fa53a1f0b81cf96a0726e2bb6e5af0d64f
Tofutush <siroccoxxq@gmail.com>
2026-03-27 16:46:37 -0400

    same

--- js/graph.js
+++ js/graph.js
@@ -379,13 +379,3 @@ document.getElementById('connections-mode').onchange = e => {
 	focusDiv.style.display = 'none';
 	connectionsDiv.style.display = 'block';
 }
-
-function elt(type, props, ...children) {
-	let dom = document.createElement(type);
-	if (props) Object.assign(dom, props);
-	for (let child of children) {
-		if (typeof child != "string") dom.appendChild(child);
-		else dom.appendChild(document.createTextNode(child));
-	}
-	return dom;
-}
