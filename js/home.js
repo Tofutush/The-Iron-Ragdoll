8908c88a31135d28f4d86c6914cace06e325264d
Tofutush <Tofutush@users.noreply.github.com>
2026-03-30 04:09:08 +0000

    deploy: fbe59f421280062649a061a80914f03faaa1c703

---
+++ js/home.js
@@ -0,0 +1,27 @@
+const keys = Object.keys(imgs);
+const div = document.getElementById('scroll-content');
+function randomize() {
+	let selected = [];
+	div.innerHTML = '';
+	while (selected.length < 50) {
+		let rand = Math.floor(Math.random() * (keys.length - 1));
+		if (!selected.includes(keys[rand])) selected.push(keys[rand]);
+	}
+	for (let z = 0; z < 2; z++)
+		for (let ch of selected) {
+			let capitalized = ch[0].toUpperCase() + ch.slice(1);
+			div.appendChild(elt('a', { href: `characters/${ch}/` },
+				elt('img', { src: imgs[ch].slice(1), alt: capitalized, title: capitalized })
+			));
+		}
+}
+randomize();
+function elt(type, props, ...children) {
+	let dom = document.createElement(type);
+	if (props) Object.assign(dom, props);
+	for (let child of children) {
+		if (typeof child == "string") dom.appendChild(document.createTextNode(child));
+		else dom.appendChild(child);
+	}
+	return dom;
+}
