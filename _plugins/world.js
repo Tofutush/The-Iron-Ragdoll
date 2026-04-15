36888091a3aceb44cfe67a06f597010b7cfa06f8
Tofutush <siroccoxxq@gmail.com>
2026-04-14 22:02:24 -0400

    that was WEIRD

--- _plugins/world.js
+++ _plugins/world.js
@@ -4,13 +4,9 @@ function worldPlugin(eleventyConfig) {
 	);
 	eleventyConfig.addCollection("worldCats", collection => {
 		let pages = collection.getFilteredByGlob('tir/world/**/*');
-		let tags = [];
-		for (let z = 0; z < pages.length; z++) {
-			if (!pages[z].data.categories) continue;
-			for (let a of pages[z].data.categories) tags.push(a);
-		}
-		tags = [...new Set(tags)].sort((a, b) => a.localeCompare(b));
-		return tags;
+		console.log([...new Set([].concat(...pages.map(p => p.data.categories || [])))].sort((a, b) => a.localeCompare(b)));
+
+		return [...new Set([].concat(...pages.map(p => p.data.categories || [])))].sort((a, b) => a.localeCompare(b));
 	});
 	eleventyConfig.addFilter('filterWorldByTag', function (arr, tag) {
 		return arr.filter(a => a.data.categories?.includes(tag));
