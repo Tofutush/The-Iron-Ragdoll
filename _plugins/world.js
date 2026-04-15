e7598330e4331b2a3e92f80590d437495ce4e2a6
Tofutush <siroccoxxq@gmail.com>
2026-04-14 22:06:24 -0400

    rm log

--- _plugins/world.js
+++ _plugins/world.js
@@ -4,8 +4,6 @@ function worldPlugin(eleventyConfig) {
 	);
 	eleventyConfig.addCollection("worldCats", collection => {
 		let pages = collection.getFilteredByGlob('tir/world/**/*');
-		console.log([...new Set([].concat(...pages.map(p => p.data.categories || [])))].sort((a, b) => a.localeCompare(b)));
-
 		return [...new Set([].concat(...pages.map(p => p.data.categories || [])))].sort((a, b) => a.localeCompare(b));
 	});
 	eleventyConfig.addFilter('filterWorldByTag', function (arr, tag) {
