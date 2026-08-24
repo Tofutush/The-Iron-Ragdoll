ea0921457895d9cbaab4af42b7d1aeba929602b4
Tofutush <siroccoxxq@gmail.com>
2026-08-24 14:49:24 +0800

    complete list gen

--- gen.py
+++ gen.py
@@ -25,29 +25,43 @@ con = [
     "ch",
     "sh",
 ]
-for z in range(50):
-    length = random.randint(1, 4)
-    word = ""
-    for z in range(length):
-        kind = random.randint(1, 9)
-        if kind == 1:
-            word += vow[random.randint(0, len(vow) - 1)]
-        elif kind == 2 or kind == 3 or kind == 4:
-            word += con[random.randint(0, len(con) - 1)]
-            word += vow[random.randint(0, len(vow) - 1)]
-        elif kind == 5 or kind == 6:
-            word += vow[random.randint(0, len(vow) - 1)]
-            letter = con[random.randint(0, len(con) - 1)]
-            while letter in ["y", "w", "l"]:
-                letter = con[random.randint(0, len(con) - 1)]
-            word += letter
-        else:
-            word += con[random.randint(0, len(con) - 1)]
-            word += vow[random.randint(0, len(vow) - 1)]
-            letter = con[random.randint(0, len(con) - 1)]
-            while letter in ["y", "w", "l"]:
-                letter = con[random.randint(0, len(con) - 1)]
-            word += letter
-        if z != length - 1:
-            word += "'"
-    print(word)
+
+final = "a'e'i'o'u"
+for z in vow:
+    for x in con:
+        if not x in ["y", "w", "l"]:
+            final += "'" + z + x
+for z in con:
+    for x in vow:
+        final += z + x
+        for c in con:
+            if not c in ["y", "w", "l"]:
+                final += z + x + c
+print(final)
+
+# for z in range(50):
+#     length = random.randint(1, 4)
+#     word = ""
+#     for z in range(length):
+#         kind = random.randint(1, 9)
+#         if kind == 1:
+#             word += vow[random.randint(0, len(vow) - 1)]
+#         elif kind == 2 or kind == 3 or kind == 4:
+#             word += con[random.randint(0, len(con) - 1)]
+#             word += vow[random.randint(0, len(vow) - 1)]
+#         elif kind == 5 or kind == 6:
+#             word += vow[random.randint(0, len(vow) - 1)]
+#             letter = con[random.randint(0, len(con) - 1)]
+#             while letter in ["y", "w", "l"]:
+#                 letter = con[random.randint(0, len(con) - 1)]
+#             word += letter
+#         else:
+#             word += con[random.randint(0, len(con) - 1)]
+#             word += vow[random.randint(0, len(vow) - 1)]
+#             letter = con[random.randint(0, len(con) - 1)]
+#             while letter in ["y", "w", "l"]:
+#                 letter = con[random.randint(0, len(con) - 1)]
+#             word += letter
+#         if z != length - 1:
+#             word += "'"
+#     print(word)
